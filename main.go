f594680801547017139ecfbb8180cfea021aef29
Lewis Silletto <sillle14@gmail.com>
2022-06-17 08:36:09 -0400

    edit and create

--- main.go
+++ main.go
@@ -13,8 +13,14 @@ import (
 
 // TODO:
 // - re organize
-// - get css started
+// - better follow rest, all endpoints should be /recipes
+//    - maybe / redirects to /recipes?
 // - form and post endpoint
+// - super simple auth, ask for a password and then send a signed JWT in a cookie. Check on all, else redirect to `/login`
+// - figure out the log message about trust all proxies
+// - Home button
+// - content -> instructions
+// - name -> something else
 
 func formatObjId(objId primitive.ObjectID) string {
 	return objId.Hex()
@@ -28,6 +34,7 @@ func createMyRender() multitemplate.Renderer {
 	// TODO: Loop through as there are more templates?
 	r.AddFromFilesFuncs("index", funcMap, "templates/base.html.tmpl", "templates/index.html.tmpl")
 	r.AddFromFilesFuncs("recipe", funcMap, "templates/base.html.tmpl", "templates/recipe.html.tmpl")
+	r.AddFromFilesFuncs("form", funcMap, "templates/base.html.tmpl", "templates/form.html.tmpl")
 	return r
 }
 
@@ -40,50 +47,109 @@ func main() {
 
 	router.HTMLRender = createMyRender()
 
-	// router.Static("/assets", "./assets") Serve static files
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	router.GET("/", func(c *gin.Context) {
-		opts := options.Find().SetProjection(bson.D{primitive.E{Key: "name", Value: "1"}})
+	router.Static("/assets", "./assets")
+	router.GET("/", func(ctx *gin.Context) {
+		opts := options.Find().SetProjection(bson.D{{Key: "name", Value: 1}})
 		opts.SetLimit(100)
-		cursor, err := RecipesCollection.Find(c.Request.Context(), bson.D{}, opts)
+		cursor, err := RecipesCollection.Find(ctx.Request.Context(), bson.D{}, opts)
 		if err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
+			ctx.AbortWithError(http.StatusInternalServerError, err)
 			return
 		}
-		var recipes []RecipeLimited
-		// TODO: Need to decode ID into string
-		if err = cursor.All(c.Request.Context(), &recipes); err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
+		var recipes []Recipe
+		if err = cursor.All(ctx.Request.Context(), &recipes); err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
 			return
 		}
 
-		c.HTML(http.StatusOK, "index", gin.H{
-			"Title":   "Home",
+		// TODO: This means that new recipes show in the index when added.
+		// TODO: How to just add this header after a new one is added?
+		ctx.Header("Cache-Control", "no-cache, must-revalidate, no-store")
+		ctx.HTML(http.StatusOK, "index", gin.H{
 			"Recipes": recipes,
 		})
 	})
 
-	router.GET("/:id", func(c *gin.Context) {
-		id := c.Param("id")
+	router.GET("/:id", func(ctx *gin.Context) {
+		id := ctx.Param("id")
 		objId, err := primitive.ObjectIDFromHex(id)
 		if err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
+			ctx.AbortWithError(http.StatusInternalServerError, err)
 			return
 		}
-		var recipe RecipeLimited
-		err = RecipesCollection.FindOne(c.Request.Context(), bson.M{"_id": objId}).Decode(&recipe)
+		var recipe Recipe
+		err = RecipesCollection.FindOne(ctx.Request.Context(), bson.M{"_id": objId}).Decode(&recipe)
 		if err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
+			ctx.AbortWithError(http.StatusInternalServerError, err)
 			return
 		}
 
-		c.HTML(http.StatusOK, "recipe", gin.H{
+		ctx.HTML(http.StatusOK, "recipe", gin.H{
 			"Recipe": recipe,
 		})
+	})
+
+	router.GET("/new", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "form", gin.H{
+			"Title": "New Recipe",
+		})
+	})
+
+	router.POST("/recipes", func(ctx *gin.Context) {
+		var recipe RecipeForm
+		err := ctx.ShouldBind(&recipe)
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		res, err := RecipesCollection.InsertOne(ctx.Request.Context(), recipe)
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		ctx.Redirect(http.StatusFound, "/" + res.InsertedID.(primitive.ObjectID).Hex())
+	})
+
+	router.GET("/:id/edit", func(ctx *gin.Context) {
+		id := ctx.Param("id")
+		objId, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		var recipe Recipe
+		err = RecipesCollection.FindOne(ctx.Request.Context(), bson.M{"_id": objId}).Decode(&recipe)
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		ctx.HTML(http.StatusOK, "form", gin.H{
+			"Title": "Edit Recipe",
+			"Recipe": recipe,
+			"Edit": true,
+		})
+	})
+
+	router.POST("/recipes/:id", func(ctx *gin.Context) {
+		id := ctx.Param("id")
+		objId, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		var recipe RecipeForm
+		err = ctx.ShouldBind(&recipe)
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		update := bson.D{{Key: "$set", Value: recipe}}
+		_, err = RecipesCollection.UpdateByID(ctx.Request.Context(), objId, update)
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		ctx.Redirect(http.StatusFound, "/" + id)
 	})
 
 	router.Run()
--- recipe.go
+++ recipe.go
@@ -3,7 +3,14 @@ package main
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // TODO: This could maybe work with a full model using omitempty
-type RecipeLimited struct {
+
+type RecipeForm struct {
+	Name string `json:"name" form:"name"`
+	Content string `json:"content" form:"content"`
+}
+
+type Recipe struct {
 	Id   primitive.ObjectID `json:"_id" bson:"_id"`
-	Name string             `json:"name"`
+	Name string             `json:"name" form:"name"`
+	Content string          `json:"content" form:"content"`
 }
